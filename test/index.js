34a6e43dc9b81d7de659db2b95c07105da9556af
Eran Hammer <eran@hammer.io>
2017-09-26 01:42:51 -0700

    New style

--- test/index.js
+++ test/index.js
@@ -16,9 +16,7 @@ const internals = {};
 
 // Test shortcuts
 
-const lab = exports.lab = Lab.script();
-const describe = lab.describe;
-const it = lab.it;
+const { describe, it } = exports.lab = Lab.script();
 const expect = Code.expect;
 
 

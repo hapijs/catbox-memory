e3f3fd223471ac69961bfba9135c4c7b2305ace0
Eran Hammer <eran@hammer.io>
2019-08-13 20:09:54 +0000

    Fix test

--- test/index.js
+++ test/index.js
@@ -124,7 +124,7 @@ describe('Memory', () => {
         const value = { a: 1 };
 
         value.b = value;
-        await expect(client.set(key, value, 10)).to.reject('Converting circular structure to JSON');
+        await expect(client.set(key, value, 10)).to.reject(/Converting circular structure to JSON/);
     });
 
     it('ignored starting a connection twice chained', async () => {
